fb8145b5fdbc90964c055f10d44b948a1273780c
Sulverus <sulverus@gmail.com>
2015-11-26 21:24:29 +0300

    Debian package deps support (equivs + mk-build-deps)

--- pack/build_deb.sh
+++ pack/build_deb.sh
@@ -1,6 +1,13 @@
 git config --global user.email "builder@tarantool.org"
 git config --global user.name "Builder"
 
+# Install build deps
+git clone -b $3 $5
+cd $4
+sudo mk-build-deps -i --tool "apt-get -y"
+sudo rm -f *.deb
+cd ../
+
 #sudo pip install debuilder
 python result/build_py.py $1 $2 $3 $4 $5
 
