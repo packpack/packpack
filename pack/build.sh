657f23d083fae503da1d1b2baa4db34a72666a94
Sulverus <sulverus@gmail.com>
2015-12-01 19:46:18 +0300

    Lua rocks detection and build rpm/deb

--- pack/build.sh
+++ pack/build.sh
@@ -4,21 +4,55 @@ branch=$1
 git_url=$2
 project=$3
 
+taranrocks_repo="https://github.com/bigbes/taranrocks.git"
+
+luarocks_export(){
+    sudo cp *.rpm ../result
+}
+
+luarocks_rpm(){
+    echo '[Lua module detected]'
+    git clone "${taranrocks_repo}"
+    yum install lua
+    ./taranrocks/src/bin/luarocks build --build-rpm `ls *.rockspec | head -n 1`
+}
+
+common_export(){
+    echo '[Common build]'
+    # move source rpm
+    sudo mv -f /home/rpm/rpmbuild/SRPMS/*.src.rpm result/
+
+    # move rpm, devel, debuginfo
+    sudo mv -f /home/rpm/rpmbuild/RPMS/x86_64/*.rpm result/
+    sudo mv -f /home/rpm/rpmbuild/RPMS/noarch/*.rpm result/
+}
+
+common_rpm(){
+    # create tarball
+    tar cvf `cat rpm/${project}.spec | grep Version: |sed -e  's/Version: //'`.tar.gz . --exclude=.git
+
+    # install build deps
+    sudo yum-builddep -y rpm/$project.spec
+
+    cp *.tar.gz ../rpmbuild/SOURCES/
+    cp -f rpm/*.ini ../rpmbuild/SOURCES/
+    rpmbuild -ba rpm/$project.spec
+}
+
 git clone -b $branch $git_url
 cd $project
 git submodule update --init --recursive
-tar cvf `cat rpm/${project}.spec | grep Version: |sed -e  's/Version: //'`.tar.gz . --exclude=.git
-sudo yum-builddep -y rpm/$project.spec
 
-cp *.tar.gz ../rpmbuild/SOURCES/
-cp -f rpm/*.ini ../rpmbuild/SOURCES/
-rpmbuild -ba rpm/$project.spec
-cd ../
+if [ -d rpm ] ; then
+    common_rpm
+    cd ../
+    common_export
+elif [ `ls -1 | grep *.rockspec | wc -l` != "0" ] ; then
+    luarocks_rpm
+    luarocks_export
+    cd ../
+else
+    echo 'Nothing to build'
+fi
 
-# move source rpm
-sudo mv -f /home/rpm/rpmbuild/SRPMS/*.src.rpm result/
-
-# move rpm, devel, debuginfo
-sudo mv -f /home/rpm/rpmbuild/RPMS/x86_64/*.rpm result/
-sudo mv -f /home/rpm/rpmbuild/RPMS/noarch/*.rpm result/
 ls -liah result
--- pack/build_deb.sh
+++ pack/build_deb.sh
@@ -1,21 +1,40 @@
 git config --global user.email "builder@tarantool.org"
 git config --global user.name "Builder"
 
+taranrocks_repo="https://github.com/bigbes/taranrocks.git"
+
+luarocks_export(){
+    sudo cp *.deb ../result
+}
+
+luarocks_deb(){
+    echo '[Lua module detected]'
+    sudo apt-get install  -y lua5.1 rpm alien
+    git clone "${taranrocks_repo}"
+    ./taranrocks/src/bin/luarocks build --build-deb `ls *.rockspec | head -n 1`
+}
+
 # Install build deps
 git clone -b $3 $5
-cd $4
-sudo mk-build-deps -i --tool "apt-get -y"
-sudo rm -f *.deb
-cd ../
+if [ -d $4/debian ] ; then
+    cd $4
+    sudo mk-build-deps -i --tool "apt-get -y"
+    sudo rm -f *.deb
+    cd ../
 
-#sudo pip install debuilder
-python result/build_py.py $1 $2 $3 $4 $5
+    python result/build_py.py $1 $2 $3 $4 $5
+    ls -liah distros/$2_amd64/builddir
 
-ls -liah distros/$2_amd64/builddir
-
-sudo mv -f distros/$2_amd64/builddir/*.deb result/
-sudo mv -f distros/$2_amd64/builddir/*.dsc result/
-sudo mv -f distros/$2_amd64/builddir/*.gz result/
-sudo mv -f distros/$2_amd64/builddir/*.xz result/
+    sudo mv -f distros/$2_amd64/builddir/*.deb result/
+    sudo mv -f distros/$2_amd64/builddir/*.dsc result/
+    sudo mv -f distros/$2_amd64/builddir/*.gz result/
+    sudo mv -f distros/$2_amd64/builddir/*.xz result/
+elif [ `ls -1 $4/ | grep .rockspec | wc -l` != "0" ] ; then
+    luarocks_deb
+    luarocks_export
+    cd ../
+else
+    echo 'Nothing to build'
+fi
 
 ls -liah result
