9db40badd868fa1cfc7a092b2c963afeacecbbe7
Roman Tsisyk <roman@tsisyk.com>
2015-12-04 15:20:49 +0300

    Tune up shell scripts

--- pack/travis.sh
+++ pack/travis.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
-if [ $PACK != 'none' ]; then
+
+SCRIPT=$(readlink -f $0)
+SCRIPT_DIR=$(dirname $SCRIPT)
+
+if [ -n "$PACK" ] && [ "$PACK" != "none" ]; then
+    [ -z $OS ] && echo "Missing OS" && exit 1
+    [ -z $DIST ] && echo "Missing DIST" && exit 1
     echo 'Test skipped(packaging mode)'
-    cd build/pack
-    bash pack.sh $PACK $OS $DIST $TRAVIS_BRANCH $OUT_REPO $PROJECT $GIT_REPO
+    cd ${SCRIPT_DIR}
+    ./pack.sh $PACK $OS $DIST $TRAVIS_BRANCH $OUT_REPO $PROJECT $GIT_REPO
 else
     echo 'Preparing tests'
     echo 'Generic tests not implemented..'
