3ce545d5a58e4784cccfe332d15f8fd6c92eb6a3
Roman Tsisyk <roman@tsisyk.com>
2015-12-04 16:07:47 +0300

    Set default values for variables

--- pack/travis.sh
+++ pack/travis.sh
@@ -6,9 +6,18 @@ SCRIPT_DIR=$(dirname $SCRIPT)
 if [ -n "$PACK" ] && [ "$PACK" != "none" ]; then
     [ -z $OS ] && echo "Missing OS" && exit 1
     [ -z $DIST ] && echo "Missing DIST" && exit 1
+    [ -z $TRAVIS_REPO_SLUG ] && echo "Missing TRAVIS_REPO_SLUG" && exit 1
+    [ -z $TRAVIS_BRANCH ] && echo "Missing TRAVIS_BRANCH" && exit 1
+
+    TRAVIS_REPO_USER=$(echo $TRAVIS_REPO_SLUG | cut -d '/' -f 1)
+    TRAVIS_REPO_NAME=$(echo $TRAVIS_REPO_SLUG | cut -d '/' -f 2)
+    [ -z $OUT_REPO ] && OUT_REPO="${TRAVIS_REPO_USER}/${TRAVIS_BRANCH}"
+    GIT_REPO=https://github.com/$TRAVIS_REPO_SLUG.git
+
     echo 'Test skipped(packaging mode)'
     cd ${SCRIPT_DIR}
-    ./pack.sh $PACK $OS $DIST $TRAVIS_BRANCH $OUT_REPO $PROJECT $GIT_REPO
+    ./pack.sh $PACK $OS $DIST $TRAVIS_BRANCH $OUT_REPO $TRAVIS_REPO_NAME \
+        $GIT_REPO
 else
     echo 'Preparing tests'
     echo 'Generic tests not implemented..'
