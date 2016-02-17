0c2830fd1bbdb0e1b0f765842db8104093780e3e
Roman Tsisyk <roman@tsisyk.com>
2016-02-17 17:46:07 +0300

    Update dependecies for LuaRocks modules

--- pack/luarocks_deps_deb.sh
+++ pack/luarocks_deps_deb.sh
@@ -3,35 +3,19 @@
 ROCKSPEC=$1
 
 tarantool_install(){
-    curl -s https://packagecloud.io/install/repositories/tarantool/1_6/script.deb.sh | sudo bash
-    sudo apt-get install -y tarantool tarantool-dev --force-yes
+    sudo apt-get install -y tarantool-dev
 }
 
-pg_install(){
-    sudo apt-get install -y libpq-dev
-}
-
-mysql_install(){
-    sudo apt-get install -y libmysqlclient-dev
-}
 libsmall_install(){
-    curl -s https://packagecloud.io/install/repositories/tarantool/1_6/script.deb.sh | sudo bash
-    sudo apt-get install -y libsmall libsmall-dev
+    sudo apt-get install -y libsmall-dev
 }
 
 libmsgpuck_install(){
-    curl -s https://packagecloud.io/install/repositories/tarantool/master/script.deb.sh | sudo bash
     sudo apt-get install -y libmsgpuck-dev
 }
 
 if [ `grep TARANTOOL ${ROCKSPEC} | wc -l` != "0" ] ; then
     tarantool_install
-fi
-if [ `grep POSTGRESQL ${ROCKSPEC} | wc -l` != "0" ] ; then
-    pg_install
-fi
-if [ `grep MYSQL ${ROCKSPEC} | wc -l` != "0" ] ; then
-    mysql_install
 fi
 if [ `grep SMALL ${ROCKSPEC} | wc -l` != "0" ] ; then
     libsmall_install
