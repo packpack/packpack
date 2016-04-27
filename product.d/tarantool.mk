8ac65ebbf603cbd06e8675d71dcba595e028350c
Roman Tsisyk <roman@tsisyk.com>
2016-04-27 21:40:39 +0300

    Fixes for Phia

--- product.d/tarantool.mk
+++ product.d/tarantool.mk
@@ -7,7 +7,9 @@ DEB_VERSION:=$(shell cat VERSION | sed -n 's/^\([0-9\.]*\)-\([0-9]*\)-\([a-z0-9]
 TARBALL_EXTRA_ARGS:= --exclude=doc/www --exclude=doc/sphinx
 # Sophia has buggy .gitignore, so sophia.c(.h) added explicitly
 # VERSION added explicitly, because it excluded by .gitignore
+ifneq ("$(wildcard ./third_party/sophia/sophia/sophia/sophia.c)","")
 define TARBALL_EXTRA_FILES
 ./third_party/sophia/sophia/sophia/sophia.c \
 ./third_party/sophia/sophia/sophia/sophia.h
 endef
+endif
