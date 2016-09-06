401ccbb43b884150023e38e89866a3a55fccfe20
Roman Tsisyk <roman@tsisyk.com>
2016-09-06 13:45:43 +0300

    Remove workaround for Sophia

--- product.d/tarantool.mk
+++ product.d/tarantool.mk
@@ -5,11 +5,3 @@ RELEASE:=1
 DEB_VERSION:=$(shell cat VERSION | sed -n 's/^\([0-9\.]*\)-\([0-9]*\)-\([a-z0-9]*\)/\1.\2.\3/p')
 
 TARBALL_EXTRA_ARGS:= --exclude=doc/www --exclude=doc/sphinx
-# Sophia has buggy .gitignore, so sophia.c(.h) added explicitly
-# VERSION added explicitly, because it excluded by .gitignore
-ifneq ("$(wildcard ./third_party/sophia/sophia/sophia/sophia.c)","")
-define TARBALL_EXTRA_FILES
-./third_party/sophia/sophia/sophia/sophia.c \
-./third_party/sophia/sophia/sophia/sophia.h
-endef
-endif
