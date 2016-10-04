3240d856c000b478397a0ea30104b24c4565ac05
Roman Tsisyk <roman@tsisyk.com>
2016-10-04 08:17:23 +0300

    Remove old make-based script

--- product.d/tarantool.mk
+++
@@ -1,7 +0,0 @@
-# Add commit number to the version string
-VERSION:=$(shell cat VERSION | sed -n 's/^\([0-9\.]*\)-\([0-9]*\)-\([a-z0-9]*\)/\1.\2/p')
-RELEASE:=1
-# Add git hash in Debian to follow convention used by official packages
-DEB_VERSION:=$(shell cat VERSION | sed -n 's/^\([0-9\.]*\)-\([0-9]*\)-\([a-z0-9]*\)/\1.\2.\3/p')
-
-TARBALL_EXTRA_ARGS:= --exclude=doc/www --exclude=doc/sphinx
